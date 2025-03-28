5dddb4cea7d0356b6632d9ef095c2819f7cbb2e4
Andrii Petrenko <andrii.petrenko@hrforecast.de>
2025-03-28 18:01:50 +0200

    Fixes to the database interface

--- nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
+++ nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -1,19 +1,25 @@
 return {
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBuffer",
-		},
-		init = function()
-			vim.g.db_ui_use_nerd_fonts = 1
-		end,
-		vim.keymap.set("n", "<leader>db", ":DBUI<CR>", {}),
-	},
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { "tpope/vim-dadbod",                     lazy = true },
+            { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+        },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+        vim.keymap.set("n", "<leader>db", ":DBUI<CR>", {}),
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "sql", "mysql", "plsql" },
+            callback = function()
+                require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+            end,
+        }),
+    },
 }
