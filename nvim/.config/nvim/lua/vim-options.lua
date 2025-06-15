5b54949ccd245617392e05d455a325a7869fb5b4
Andrii Petrenko <andrii.petrenko@hrforecast.de>
2025-06-15 17:21:28 +0300

    Added symbols-outline and telescope symbols finder

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -15,3 +15,4 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 --Reset hightlighting after the search
 vim.keymap.set('n', '<leader>n', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", { desc = "Find symbols in file" })
