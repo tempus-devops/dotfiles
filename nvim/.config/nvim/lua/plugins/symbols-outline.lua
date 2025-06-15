e363c89511e0d52b40605a068dfd521ec2822732
Andrii Petrenko <andrii.petrenko@hrforecast.de>
2025-06-15 13:47:44 +0300

    Added symbols outline plugin

---
+++ nvim/.config/nvim/lua/plugins/symbols-outline.lua
@@ -0,0 +1,19 @@
+return {
+    "simrat39/symbols-outline.nvim",
+    cmd = { "SymbolsOutline", "SymbolsOutlineOpen" },
+    keys = {
+        { "<leader>so", "<cmd>SymbolsOutline<CR>", desc = "Symbols Outline" },
+    },
+    config = function()
+        require("symbols-outline").setup({
+            highlight_hovered_item = true,
+            show_guides = true,
+            auto_preview = false,
+            position = "right",
+            width = 35,
+            show_numbers = false,
+            show_relative_numbers = false,
+            show_symbol_details = true,
+        })
+    end,
+}
