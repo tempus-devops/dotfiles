f50cc15b605bce8920e3fcf5597ce6f6e030e2e4
Andrii Petrenko <andrii.petrenko@hrforecast.de>
2025-10-21 15:11:09 +0500

    Prepared lsp-config for the neovim 0.11.3

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,73 +1,72 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	config = function()
-		local navic = require("nvim-navic") -- <--- require navic here
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "dockerls",
+          "docker_compose_language_service",
+          "bashls",
+          "html",
+          "helm_ls",
+          "jsonls",
+          "jedi_language_server",
+          "sqlls",
+          "terraformls",
+          "tflint",
+          "lemminx",
+          "yamlls",
+          "phpactor",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      if not (vim.lsp and vim.lsp.config and vim.lsp.enable) then
+        vim.notify("This config requires Neovim >= 0.11.3", vim.log.levels.ERROR)
+        return
+      end
 
-		local on_attach = function(client, bufnr)
-			if client.server_capabilities.documentSymbolProvider then
-				navic.attach(client, bufnr)
-			end
-		end
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		local lspconfig = require("lspconfig")
+      local servers = {
+        "lua_ls",
+        "dockerls",
+        "docker_compose_language_service",
+        "bashls",
+        "html",
+        "helm_ls",
+        "jsonls",
+        "jedi_language_server",
+        "sqlls",
+        "terraformls",
+        "tflint",
+        "lemminx",
+        "yamlls",
+        "phpactor",
+      }
 
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.dockerls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.docker_compose_language_service.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.bashls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.html.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.helm_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.jsonls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.jedi_language_server.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.sqlls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.terraformls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.lemminx.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.yamlls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-		lspconfig.phpactor.setup({
-			capabilities = capabilities,
-			on_attach = on_attach, -- ✅ Attach navic here
-		})
+      for _, name in ipairs(servers) do
+        vim.lsp.config(name, {
+          capabilities = capabilities,
+        })
+        vim.lsp.enable(name)
+      end
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-	end,
+      -- Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP Goto Definition" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+    end,
+  },
 }
